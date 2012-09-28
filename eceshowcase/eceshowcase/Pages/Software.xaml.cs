86706c620e57d395cabe7da67f2d8ab4eb1581bc
Harrison Kim <email.harrison.kim@gmail.com>
2012-09-28 15:45:19 +1200

    Change Software to UI in detail

--- eceshowcase/eceshowcase/Pages/Software.xaml.cs
+++ eceshowcase/eceshowcase/Pages/Software.xaml.cs
@@ -11,22 +11,114 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
 
 namespace eceshowcase.Pages
 {
     /// <summary>
-    /// Interaction logic for Electrical.xaml
+    /// Interaction logic for SoftwareEngineeringPage.xaml
     /// </summary>
     public partial class Software : UserControl
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public Software()
         {
             Switcher.pageSwitcher.TransitionType = PageTransitionType.SlideAndFadeLeft;
 
             InitializeComponent();
+
+            // Add handlers for window availability events
+            AddWindowAvailabilityHandlers();
+            Overview.Visibility = Visibility.Visible;
+            Courses.Visibility = Visibility.Hidden;
+            Faculty.Visibility = Visibility.Hidden;
         }
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        /// <summary>
+        /// Adds handlers for window availability events.
+        /// </summary>
+        private void AddWindowAvailabilityHandlers()
+        {
+            // Subscribe to surface window availability events
+            ApplicationServices.WindowInteractive += OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// Removes handlers for window availability events.
+        /// </summary>
+        private void RemoveWindowAvailabilityHandlers()
+        {
+            // Unsubscribe from surface window availability events
+            ApplicationServices.WindowInteractive -= OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// This is called when the user can interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowInteractive(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when the user can see but not interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowNoninteractive(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        /// This is called when the application's window is not visible or interactive.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowUnavailable(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+
+        private void ShowOverview()
+        {
+            Overview.Visibility = Visibility.Visible;
+            Courses.Visibility = Visibility.Hidden;
+            Faculty.Visibility = Visibility.Hidden;
+        }
+        private void OverView_Click(object sender, RoutedEventArgs e)
+        {
+            ShowOverview();
+        }
+
+        private void Courses_Click(object sender, RoutedEventArgs e)
+        {
+            Overview.Visibility = Visibility.Hidden;
+            Courses.Visibility = Visibility.Visible;
+            Faculty.Visibility = Visibility.Hidden;
+        }
+
+        private void Faculty_Click(object sender, RoutedEventArgs e)
+        {
+            Overview.Visibility = Visibility.Hidden;
+            Courses.Visibility = Visibility.Hidden;
+            Faculty.Visibility = Visibility.Visible;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new MainMenu());
         }
