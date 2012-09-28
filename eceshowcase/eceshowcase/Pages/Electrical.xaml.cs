2775b6d4082653d2270a773d666227118ebd0c45
Harrison Kim <email.harrison.kim@gmail.com>
2012-09-28 13:59:53 +1200

    Make base program with page switching and transition animations

---
+++ eceshowcase/eceshowcase/Pages/Electrical.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eceshowcase.Pages
+{
+    /// <summary>
+    /// Interaction logic for Electrical.xaml
+    /// </summary>
+    public partial class Electrical : UserControl
+    {
+        public Electrical()
+        {
+            InitializeComponent();
+            Switcher.pageSwitcher.TransitionType = PageTransitionType.SlideAndFade;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Switcher.Switch(new MainMenu());
+        }
+    }
+}
