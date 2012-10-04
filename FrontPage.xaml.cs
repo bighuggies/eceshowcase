7dea1970cbfeb77d821c83f11199367d6f4e3fb6
Andrew Hughson <andrew@atshughson.me>
2012-10-04 13:52:30 +1300

    Implement swiping between carousel pages. Andrew + Jess

--- FrontPage.xaml.cs
+++ FrontPage.xaml.cs
@@ -19,6 +19,9 @@ namespace eceshowcase
     /// </summary>
     public partial class FrontPage : Page
     {
+
+        private Dictionary<TouchDevice, Point> currentTouchDevices = new Dictionary<TouchDevice, Point>();
+
         private ShowcaseWindow window;
 
         /// <summary>
@@ -38,6 +41,47 @@ namespace eceshowcase
         private void map_btn_Click(object sender, RoutedEventArgs e)
         {
             window.ShowPage(new MapPage(window));
+        }
+
+        private void welcome_carousel_TouchMove(object sender, TouchEventArgs e)
+        {
+            if (currentTouchDevices.Count == 1)
+            {
+                int isLeft = 0;
+                int isRight = 0;
+                foreach (KeyValuePair<TouchDevice, Point> td in currentTouchDevices)
+                {
+                    if (td.Key != null && e.TouchDevice.GetTouchPoint(this).Position.X - td.Value.X > 100)
+                        isRight++;
+                    else if (td.Key != null && td.Value.X - e.TouchDevice.GetTouchPoint(this).Position.X > 100)
+                        isLeft++;
+                    else
+                        return;
+                }
+
+                if (isLeft == 1 && isRight == 0)
+                {
+                    welcome_carousel.NextPage();
+                    currentTouchDevices.Clear();
+                    return;
+                }
+                else if (isRight == 1 && isLeft == 0)
+                {
+                    welcome_carousel.PreviousPage();
+                    currentTouchDevices.Clear();
+                    return;
+                }
+            }
+        }
+
+        private void welcome_carousel_TouchDown(object sender, TouchEventArgs e)
+        {
+            currentTouchDevices.Add(e.TouchDevice, e.TouchDevice.GetTouchPoint(this).Position);
+        }
+
+        private void welcome_carousel_TouchUp(object sender, TouchEventArgs e)
+        {
+            currentTouchDevices.Remove(e.TouchDevice);
         }
     }
 }
