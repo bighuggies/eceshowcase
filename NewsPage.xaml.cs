d46cdf8849032d4bcbafa25dbcb88a9da9fe5020
Andrew Hughson <andrew@atshughson.me>
2012-10-15 20:37:17 +1300

    Change from ListBox to ItemsControl to fix selecting items

--- NewsPage.xaml.cs
+++ NewsPage.xaml.cs
@@ -17,40 +17,6 @@ using System.Windows.Media.Animation;
 
 namespace eceshowcase
 {
-    public class NewsItem
-    {
-        public string title { get; set; }
-        public string content { get; set; }
-        public string date { get; set; }
-
-        public NewsItem(string _title, string _content, string _date)
-        {
-            title = _title;
-            content = _content;
-            date = _date;
-        }
-    }
-
-    public class RSSFeed
-    {
-        public List<NewsItem> newsList { get; set; }
-
-        public RSSFeed()
-        {
-            XmlTextReader rssReader = new XmlTextReader("http://www.engineering.auckland.ac.nz/uoa/home/template/news_feed.rss");
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(rssReader);
-
-            newsList = new List<NewsItem>();
-
-            XmlNodeList n = xmlDoc.SelectNodes("//channel/item");
-            foreach (XmlNode node in n)
-            {
-                newsList.Add(new NewsItem(node["title"].InnerText, node["description"].InnerText, node["pubDate"].InnerText));
-            }
-        }
-    }
-
     /// <summary>
     /// Interaction logic for NewsPage.xaml
     /// </summary>
@@ -62,37 +28,6 @@ namespace eceshowcase
         {
             InitializeComponent();
             window = w;
-
-            RSSFeed feed = new RSSFeed();
-
-            SolidColorBrush whiteBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-
-            foreach (NewsItem item in feed.newsList)
-            {
-                Label title = new Label();
-                title.Foreground = whiteBrush;
-                title.Content = item.title;
-                title.FontSize = 26;
-                //contentPanel.Children.Add(title);
-
-
-                Label date = new Label();
-                date.Foreground = whiteBrush;
-                date.Content = item.date;
-                date.FontSize = 26;
-                //contentPanel.Children.Add(date);
-
-                TextBlock content = new TextBlock();
-                content.FontSize = 20;
-                content.Text = item.content;
-                content.Foreground = whiteBrush;
-                content.Width = 800;
-                content.TextWrapping = TextWrapping.Wrap;
-                content.TextAlignment = TextAlignment.Left;
-                content.HorizontalAlignment = HorizontalAlignment.Left;
-                content.Margin = new Thickness(20, 0, 20, 0);
-                //contentPanel.Children.Add(content);
-            }
 
             window.hidePage = (window.Resources["SlideAndFadeLeftOut"] as Storyboard).Clone();
             window.showPage = (window.Resources["SlideAndFadeLeftIn"] as Storyboard).Clone();
